2727acff4600440ac5ebd58bcedb4c2bbbd45203
Likogan <59451856+Likogann@users.noreply.github.com>
2023-02-13 21:44:27 -0700

    Update xsetroot-script.sh

--- dwm/xsetroot-script.sh
+++ dwm/xsetroot-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# use crontab for boot loading
-# @reboot /usr/local/src/dwm/xsetroot-script.sh
+# use crontab for boot loading - sadly this doesn't load if dwm crashes, or if you logout.
+# @reboot screen bash /usr/local/src/dwm/xsetroot-script.sh
 while true; do
         xsetroot -name "$(date)"
         sleep 1
